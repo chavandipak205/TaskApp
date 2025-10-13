6c9f69a50dfa7a197035201d83edac664d4f100c
chavandipak205 <chavandipak205@gmail.com>
2025-10-13 22:18:19 +0530

    files upfdayes

--- src/data/Dipak.js
+++ src/data/Dipak.js
@@ -5,6 +5,8 @@ const Dipak = () => {
     <div>
       <h1>Hello, I'm Dipak!</h1>
       <p>Welcome to my page.</p>
+      <h2>About Me</h2>
+      <p>I'm a passionate learner and I'm currently focused on improving my coding skills. Any help or resources would be greatly appreciated!</p>
     </div>
   )
 }
