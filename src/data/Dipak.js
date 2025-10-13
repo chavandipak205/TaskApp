f7b2e1af93f2d7bb38e152f39fdf3b286808d149
chavandipak205 <chavandipak205@gmail.com>
2025-10-13 22:24:01 +0530

    Added

--- src/data/Dipak.js
+++ src/data/Dipak.js
@@ -6,6 +6,7 @@ const Dipak = () => {
       <h1>Hello, I'm Dipak!</h1>
       <p>Welcome to my page.</p>
       <h2>About Me</h2>
+      <p>help me</p>
       <p>I'm a passionate learner and I'm currently focused on improving my coding skills. Any help or resources would be greatly appreciated!</p>
     </div>
   )
