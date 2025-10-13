906502c602c905375695fe272289c358276ddd98
chavandipak205 <chavandipak205@gmail.com>
2025-10-13 21:58:59 +0530

    second Commit

---
+++ src/data/Dipak.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dipak = () => {
+  return (
+    <div>
+      <h1>Hello, I'm Dipak!</h1>
+      <p>Welcome to my page.</p>
+    </div>
+  )
+}
+
+export default Dipak
